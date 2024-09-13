54d01ef15ddae87d55403fb7e8ae8c9860e3a7c4
Alexander Robert <arobert@roblox.com>
2024-09-13 16:26:30 -0700

    Renamed option to choice. Remodeled to return ChoiceImage instead of radio options

--- src/SelectionForm.jsx
+++ src/SelectionForm.jsx
@@ -1,46 +1,66 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import ChoiceImage from "./ChoiceImage";
 
-function SelectionForm({ options, onSubmit }) {
-  const [selectedOption, setSelectedOption] = useState('');
+function SelectionForm({ choices, onSubmit }) {
+  const [selectedChoice, setSelectedChoice] = useState("");
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const handleChoiceChange = (event) => {
+    setSelectedChoice(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`You selected: ${selectedOption}`);
-    if (selectedOption) {
-      onSubmit(selectedOption);
+    console.log(`You selected: ${selectedChoice}`);
+    if (selectedChoice) {
+      onSubmit(selectedChoice);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <button type="submit">Shoot!</button>
-      {options.map((option) => {
-        return (
-          <div key={option}>
-            <label>
-              <input
-                type="radio"
-                value={option}
-                checked={selectedOption === option}
-                onChange={handleOptionChange}
-              />
-              {option}
-            </label>
-          </div>
-        );
-      })}
+      <button
+        type="submit"
+        disabled={!selectedChoice}
+        style={{ margin: "10px", padding: "5px", outlineStyle: "solid" }}
+      >
+        Shoot!
+      </button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(100px, 1fr))",
+          gap: "10px",
+        }}
+      >
+        {choices.map((choice) => {
+          return (
+            <div
+              key={choice}
+              onClick={() => handleChoiceChange(choice)}
+              style={{
+                border:
+                  selectedChoice === choice
+                    ? "3px solid blue"
+                    : "1px solid gray",
+                padding: "10px",
+                cursor: "pointer",
+                textAlign: "center",
+                borderRadius: "8px",
+              }}
+            >
+              <ChoiceImage choice={choice} />
+            </div>
+          );
+        })}
+      </div>
     </form>
   );
 }
 
 // Adding PropTypes validation
 SelectionForm.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  choices: PropTypes.arrayOf(PropTypes.string).isRequired,
   onSubmit: PropTypes.func,
 };
 
